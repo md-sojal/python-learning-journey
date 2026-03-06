2a8fc1b208dc935815be032c0d352d32eecef95f
md-sojal <mdsojal2004@gmail.com>
2026-03-07 01:38:45 +0600

    Update letters.py

--- Week-01/code/letters.py
+++ Week-01/code/letters.py
@@ -1,1 +1,24 @@
+#I did this after seeing it in a short lecture on loops in CS50 Python
+def main():
+    names = {"Mario", "Luigi", "Daisy", "Yoshi"}
+    for name in names:
+        print(write_letter(name, "Princess Peach"))
 
+def write_letter(receiver, sender):
+    return f"""
+    ========================================
+    
+    Dear {receiver},
+    
+    you are cordially invited to a ball at
+    Peach's Castel this evening, 7:00 pm.
+
+
+    Sincerely,
+    {sender}
+
+    ========================================
+
+    """
+
+main()
